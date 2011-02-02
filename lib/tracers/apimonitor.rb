9f020dcf7b9af16e75e5c98ed15db56736d66c71
Jon <jon.forums@gmail.com>
2011-02-02 15:26:22 -0500

    Add stubbed APIMonitor trace provider.

---
+++ lib/tracers/apimonitor.rb
@@ -0,0 +1,17 @@
+# Copyright (c) 2011, Jon Maken
+# License: 3-clause BSD (see project LICENSE file)
+
+module RCI
+  module Tracers
+    class APIMonitor
+
+      def initialize(exe)
+        @exe = exe
+      end
+
+      def call(env)
+        raise NotImplementedError, "[TODO] implement #{self.class.name}#call(env)"
+      end
+    end
+  end
+end
