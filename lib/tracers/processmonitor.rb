7058bbef3673e32f0958f76e39c9704833c121a7
Jon <jon.forums@gmail.com>
2011-02-10 12:11:59 -0500

    Create timestamped procmon trace log files.

--- lib/tracers/processmonitor.rb
+++ lib/tracers/processmonitor.rb
@@ -12,14 +12,13 @@ module RCI
 
       def call(env)
         # TODO implement UAC check and bail out with a message to use elevated shell
-        #      generate timestamped log output files from the tracer
-        #      how to cleverly avoid tracing Ruby startup?
         # FIXME figure out why JRuby doesn't like this
 
         #puts "Ruby: %s\ndisable_gems: %s\ntracer: %s\ntarget:%s" %
         #      [ RCI.ruby, @exe, env[:disable_gems], env[:target] ]
 
-        system("start #{@exe} /quiet /minimized /backingfile #{File.join(RCI::WORLD_CONFIG[:logs_dir], 'api_trace.pml')}")
+        log_file = 'api_trace_%s.pml' % Time.now.strftime('%Y-%m-%dT%k_%M_%S')
+        system("start #{@exe} /quiet /minimized /backingfile #{File.join(RCI::WORLD_CONFIG[:logs_dir], log_file)}")
         system("#{@exe} /waitforidle")
         system("#{RCI.ruby} #{env[:disable_gems]} \"#{env[:target]}\"")
         system("#{@exe} /terminate")
