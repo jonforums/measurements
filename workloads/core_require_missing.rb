802bca4aa532b584299bb29c38f9b7e62aefd6b3
Jon <jon.forums@gmail.com>
2011-02-15 09:45:55 -0500

    Remove invalid workload; not testing exceptions.

--- workloads/core_require_missing.rb
+++
@@ -1,9 +0,0 @@
-in_dir = File.join(File.expand_path(File.dirname(__FILE__)), '..', 'input')
-
-500.times do |i|
-  begin
-    f = 'bogus%s' % i
-    require "#{File.join(in_dir, f)}"
-  rescue LoadError
-  end
-end
