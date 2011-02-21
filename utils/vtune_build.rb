8402154e83357a3a2e8249a19675a8f9198852df
Jon <jon.forums@gmail.com>
2011-02-21 15:45:25 -0500

    Update RubyInstaller VTune override sample.

--- utils/vtune_build.rb
+++ utils/vtune_build.rb
@@ -2,7 +2,5 @@
 if ENV['VTUNE_BUILD'] then
   puts '[INFO] Overriding to enable VTune profiled Ruby 1.9.x...'
 
-  RubyInstaller::Ruby19.configure_options << "--enable-vtune"
-  RubyInstaller::Ruby19.configure_options << "CPPFLAGS='-Ic:/devlibs/vtune/include'"
-  RubyInstaller::Ruby19.configure_options << "LDFLAGS='-Lc:/devlibs/vtune/lib32'"
+  RubyInstaller::Ruby19.configure_options << "--with-vtune=c:/devlibs/vtune"
 end
