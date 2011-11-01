24c23d24778e1cdf0b753d4d9981b5e559b9be2a
Jon <jon.forums@gmail.com>
2011-11-01 10:39:27 -0400

    Update VTune AmplifierXE automation support

--- lib/profilers/amplifierxe.rb
+++ lib/profilers/amplifierxe.rb
@@ -18,6 +18,7 @@ module RCI
         cmd << ' -collect hotspots -knob accurate-cpu-time-detection=true -follow-child'
         cmd << ' -target-duration-type=short -no-allow-multiple-runs -no-analyze-system'
         cmd << ' -data-limit=100 -slow-frames-threshold=40 -fast-frames-threshold=100'
+        cmd << " -search-dir=src=\"#{RCI::USER_CONFIG[:ruby_source]}\""
         cmd << " -user-data-dir=\"#{RCI::WORLD_CONFIG[:logs_dir]}\""
         cmd << " -result-dir=\"hotspots_#{Time.now.strftime('%Y-%m-%dT%H_%M_%S')}\""
         cmd << " -quiet \"#{RCI.ruby}\" #{env[:disable_gems]} \"#{env[:target]}\""
