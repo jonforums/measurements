1bbd68256450bccff796365db11070b7dc0c5800
Jon <jon.forums@gmail.com>
2011-11-03 20:43:45 -0400

    Properly clear and repopulate $LOADED_FEATURES

--- workloads/core_require_nested.rb
+++ workloads/core_require_nested.rb
@@ -2,6 +2,7 @@ in_dir = File.join(File.expand_path(File.dirname(__FILE__)), '..', 'input',
                    'empty_files', 'a', 'b', 'c', 'd', 'e')
 c = ENV['MEASURE_EXTENDED'] || 1
 
+cache = $LOADED_FEATURES.dup
 c.to_i.times do
   10.times do
     100.times do |i|
@@ -9,5 +10,6 @@ c.to_i.times do
       require "#{File.join(in_dir, f)}"
     end
     $LOADED_FEATURES.clear
+    $LOADED_FEATURES.concat(cache)
   end
 end
