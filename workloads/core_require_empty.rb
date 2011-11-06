14c59a5df174d5d4c33b9dd12729ce6dc4920575
Jon <jon.forums@gmail.com>
2011-11-05 21:32:59 -0400

    Fix `require` workloads. Thanks luislavena

--- workloads/core_require_empty.rb
+++ workloads/core_require_empty.rb
@@ -1,15 +1,19 @@
-in_dir = File.join(File.expand_path(File.dirname(__FILE__)), '..', 'input', 'empty_files')
+in_dir = File.expand_path('../input/empty_files', File.dirname(__FILE__))
 
 c = ENV['MEASURE_EXTENDED'] || 1
+
+# XXX cheat using `unshift` to jump to the front
+$LOAD_PATH.push in_dir
 
 cache = $LOADED_FEATURES.dup
 c.to_i.times do
   10.times do
     100.times do |i|
-      f = 'empty%s' % i
-      require "#{File.join(in_dir, f)}"
+      require "empty#{i}"
     end
     $LOADED_FEATURES.clear
     $LOADED_FEATURES.concat(cache)
   end
 end
+
+$LOAD_PATH.delete in_dir
--- workloads/core_require_nested.rb
+++ workloads/core_require_nested.rb
@@ -1,15 +1,19 @@
-in_dir = File.join(File.expand_path(File.dirname(__FILE__)), '..', 'input',
-                   'empty_files', 'a', 'b', 'c', 'd', 'e')
+in_dir = File.expand_path('../input/empty_files/a/b/c/d/e', File.dirname(__FILE__))
+
 c = ENV['MEASURE_EXTENDED'] || 1
+
+# XXX cheat using `unshift` to jump to the front
+$LOAD_PATH.push in_dir
 
 cache = $LOADED_FEATURES.dup
 c.to_i.times do
   10.times do
     100.times do |i|
-      f = 'nested_empty%s' % i
-      require "#{File.join(in_dir, f)}"
+      require "nested_empty#{i}"
     end
     $LOADED_FEATURES.clear
     $LOADED_FEATURES.concat(cache)
   end
 end
+
+$LOAD_PATH.delete in_dir
